03e6689f6f99b458366a58004dbd4433dace8ca0
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-04-24 08:36:53 -0500

    added module separation and html generation, end of 9.2

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const profileDataArgs = process.argv.slice(2)
+const [name, github] = profileDataArgs;
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    // This...
+fs.writeFile('index.html',  generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+})
+
+// console.log(generatePage(name, github));
+
+
+
+
+
+
+
+
+
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+//     // This...
     
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
 
-    console.log('================');
+//     console.log('================');
 
-    // Is the same as this...
+//     // Is the same as this...
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
    
 
-};
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
 
