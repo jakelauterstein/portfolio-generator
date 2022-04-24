fafdfaa4dc52237ca0241ee62b7052da66b033dd
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-04-24 10:48:42 -0500

    added first inquirer prompt, mid  9.3.5

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const fs = require('fs');
-const profileDataArgs = process.argv.slice(2)
-const [name, github] = profileDataArgs;
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-fs.writeFile('index.html',  generatePage(name, github), err => {
-    if (err) throw err;
+inquirer.
+    prompt([
+        {
+            type: 'input',
+            name: "name",
+            message: "What is your name?"
+        }
+])
+    .then(answers => console.log(answers));
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-})
 
-// console.log(generatePage(name, github));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
 
 
 
